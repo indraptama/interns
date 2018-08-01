481da0feb30d58984abc10af2077e34923aa565d
Indra Pratama Putra <indra.symfonia@gmail.com>
2018-08-01 09:26:24 +0700

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  h1 This Is Vue
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import App from './App.vue'
+
+
+// Router Setup
+Vue.use(VueRouter);
+const router = [
+
+]
+
+new Vue({
+  el: '#App',
+  render: h => h(App)
+})
